33831c0f330c192d0380c26c9f7089edbd5e3bc4
Kholismjd12 <sicepot741@gmail.com>
2024-07-03 18:10:16 +0700

    Add files via upload

--- Admin.php
+++ Admin.php
@@ -4,7 +4,7 @@ namespace App\Entities;
 
 use CodeIgniter\Entity\Entity;
 
-class Penyewa extends Entity
+class Admin extends Entity
 {
     protected $datamap = [];
     protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
