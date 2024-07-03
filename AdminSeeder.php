e9d634e8e4f0febfad1eb41f80cb9f7b7164c316
Kholismjd12 <sicepot741@gmail.com>
2024-07-03 18:11:14 +0700

    Add files via upload

---
+++ AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'username' => 'Ucup',
+                'password' => password_hash('ucup123', PASSWORD_DEFAULT),
+            ],
+            [
+                'username' => 'Sumarni',
+                'password' => password_hash('marni123', PASSWORD_DEFAULT),
+            ],
+        ];
+
+        // Using Query Builder
+        $this->db->table('admin')->insertBatch($data);
+    }
+}
